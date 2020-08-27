b9f0b21b560884a907c5d9192d7dfc4af6cee01b
MiladEsp <32962296+MiladEsp@users.noreply.github.com>
2020-08-26 22:41:48 -0400

    Added Array class and an example

---
+++ array/Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+#include "Array.h"
+
+int main()
+{   
+    // An example of the use of our Array class
+    
+    Array<int, 5> data;
+
+    Array<std::string, data.Size()> newArray;
+
+    std::memset(data.Data(), 0, data.Size() * sizeof(int));
+
+    for (size_t i = 0; i < data.Size(); i++)
+    {
+        data[i] = 2;
+        std::cout << data[i] << std::endl;
+    }
+
+    return 0;
+}
